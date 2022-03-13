fc4e44e1c05903d521c756395975a5f49096c21a
Michael DeGan <m_degan@mail.fhsu.edu>
2022-03-12 23:05:43 -0600

    Readded semicolon

--- config/Database.php
+++ config/Database.php
@@ -23,7 +23,7 @@
             $this->conn = null;
 
             try {
-                $this->conn = new PDO('mysql:host=' . this->$host . 'dbname=' . $this->$database, $this->username, $this->password);
+                $this->conn = new PDO('mysql:host=' . this->$host . ';dbname=' . $this->$database, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
